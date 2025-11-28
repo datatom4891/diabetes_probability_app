278f9824c35e2c78fec9cf08fd27f2951f73d9d6
Tom Oguntuga <adetomiwa.oguntuga@gmail.com>
2025-11-28 17:27:19 -0600

    Docker files - 11/28/2025

--- frontend/utils/functions.py
+++ frontend/utils/functions.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "http://127.0.0.1:8000"
+base_url = "http://backend:8000"
 
 def submit_prediction(input_request):
   diabetes_endpoint = f"{base_url}/diabetes/probability_ui"
